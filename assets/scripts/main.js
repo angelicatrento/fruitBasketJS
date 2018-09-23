c57fb65b3911573ae3ea22421cc39163f451ba7e
angelicagtrento <angelicagtrento@gmail.com>
2018-09-23 20:47:15 -0300

    adding final score screen
    - when the maximun number of misses is reached, the game is over and displays player score.
    - added game controls on interface

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,12 @@ var fruits = [new Fruit(),new Fruit(),new Fruit()];
 var basket;
 var numberOfFruits = 5;
 
+// =======================================
+// Centralise Canvas:
+// =======================================
+var canvas;
+
+
 function preload() {
     basket = new Basket();
     
@@ -19,7 +25,8 @@ function preload() {
 
 function setup() {
     // put setup code here
-    createCanvas(640,480);
+    canvas = createCanvas(640, 480);
+    centerCanvas();
     //scoreElem = createDiv('SCORE: ' + score);
     //scoreElem.position(20, 20);
     //scoreElem.id = 'score';
@@ -42,17 +49,38 @@ function draw() {
     colorMode(RGB);
     // change canvas backgroud color 
     background(223,225,239);
-    
-    for (count in fruits)
-    {
-        fruits[count].UpdateFruitPosition(basket.x,basket.sprite.width);
+    if(fruitsLost < maxNumberMisses){
+        for (count in fruits){
+            fruits[count].UpdateFruitPosition(basket.x,basket.sprite.width);
+        }
+
+        basket.MoveBasket();
+    }
+    else{
+        gameOver();
     }
     
-    basket.MoveBasket();
     
     //interface 
     fill(81, 43, 102)
         .textSize(15)
         .text("SCORE: " + score,10,20)
         .text("MISSES: " + fruitsLost + " of " + maxNumberMisses,10,40);
+}
+
+function centerCanvas() {
+  canvas.position((windowWidth - width) / 2, (windowHeight - height) / 3);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
+function gameOver(){
+    fill(81,43,102)
+    .textSize(30)
+    .textStyle('bold')
+    .text("YOUR FINAL SCORE IS",width/4,height/2)
+    .text(score,width/2,(height/2)+30);
+    
 }
